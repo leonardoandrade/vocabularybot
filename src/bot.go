86d66bef3fab5f7dc09c36578e8f8dcf4af7ff31
leonardoandrade <leonardo@andrade.io>
2014-07-04 21:26:07 +0200

    refactored client

--- src/bot.go
+++ src/bot.go
@@ -1,14 +1,11 @@
-/*
-* Based on example.go from github.com/mattn/go-xmpp
- */
-
 package main
 
 import (
 	"fmt"
 	"os"
     "./dictionary"
-    "./client/"
+    "./client/gtalk"
+    "time"
 )
 
 
@@ -21,17 +18,30 @@ func main() {
     }
 
     var dict dictionary.Dictionary  = dictionary.Make(os.Args[1])
-    gtalk := client.Make()
+    gtalk := gtalk.Make()
 
     go gtalk.Init()
 
+    count :=1
 	for {
+
 	       req := <- gtalk.Output
+           if count ==1 {
+               time.Sleep(time.Duration(5) * time.Second)
+           }
            response := dict.Lookup(req)
            if response == "" {
-               response = "word not found :-("
+               correction, meaning := dict.Correct(req)
+               fmt.Println("CORRECTIONL"+correction)
+               if correction != "" {
+                   response = "did you mean '"+correction+"'? that means '"+meaning+"'"
+               } else {
+                   response = "word not found :-("
+               }
            }
            fmt.Println(response)
            gtalk.Input <- response
+
+           count = count + 1
 	}
 }
